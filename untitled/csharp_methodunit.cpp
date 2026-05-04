9c59c019ea669ca8f01e31b88c74f6394e8967bf
BNS200 <nbufalov2005@gmail.com>
2026-05-04 16:08:02 +0700

    implementation of the csharpmethod namely compilemodifiers() and compilesignature()

--- untitled/csharp_methodunit.cpp
+++ untitled/csharp_methodunit.cpp
@@ -1,3 +1,30 @@
 #include "csharp_methodunit.h"
 
-CSharpMethodUnit::CSharpMethodUnit() {}
+CSharpMethodUnit::CSharpMethodUnit(const std::string& name, const std::string& returnType, Flags flags)
+    : MethodUnit(name, returnType, flags) {}
+
+std::string CSharpMethodUnit::compileModifiers() const
+{
+    std::string result;
+    if (getFlags() & STATIC) {
+        result += "static ";
+    }
+    if (getFlags() & VIRTUAL) {
+        result += "virtual ";
+    }
+    if (getFlags() & ABSTRACT) {
+        result += "abstract ";
+    }
+    if (getFlags() & SEALED) {
+        result += "sealed ";
+    }
+    if (getFlags() & OVERRIDE) {
+        result += "override ";
+    }
+    return result;
+}
+
+std::string CSharpMethodUnit::compileSignature() const
+{
+    return getReturnType() + " " + getName() + "()";
+}
--- untitled/csharp_methodunit.h
+++ untitled/csharp_methodunit.h
@@ -5,7 +5,22 @@
 class CSharpMethodUnit : public MethodUnit
 {
 public:
-    CSharpMethodUnit();
+    enum Modifier {
+        STATIC = 1,
+        VIRTUAL = 1 << 1,
+        ABSTRACT = 1 << 2,
+        SEALED = 1 << 3,
+        OVERRIDE = 1 << 4,
+        NEW = 1 << 5
+    };
+
+    explicit CSharpMethodUnit(const std::string& name, const std::string& returnType, Flags flags);
+    virtual ~CSharpMethodUnit() = default;
+
+    virtual std::string compileModifiers() const = 0;
+    virtual std::string compileSignature() const = 0;
+
+
 
 };
 
